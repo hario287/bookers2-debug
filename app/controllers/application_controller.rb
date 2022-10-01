a82dd503b68bbfdae6cd0306f1a8eff241166061
EC2 Default User <mhyrso27@gmail.com>
2022-10-01 16:06:51 +0000

    [Fin]応用2-11

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,15 +6,15 @@ class ApplicationController < ActionController::Base
     user_path(current_user.id)
   end
 
-  def after_sign_up_path_for(resource)
-    user_path(current_user.id)
-  end
+  # def after_sign_up_path_for(resource)
+    # user_path(current_user.id)
+  # end
 
   def after_sign_out_path_for(resource)
     root_path
   end
 
-private
+protected
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:email])
   end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,11 +10,14 @@ class UsersController < ApplicationController
   def index
     @users = User.all
     @book = Book.new
+  end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def update
+    @user = User.find(params[:id])
     if @user.update(user_params)
       redirect_to users_path(@user), notice: "You have updated user successfully."
     else
@@ -22,7 +25,8 @@ class UsersController < ApplicationController
     end
   end
 
-  private
+
+private
 
   def user_params
     params.require(:user).permit(:name, :introduction, :profile_image)
@@ -33,6 +37,5 @@ class UsersController < ApplicationController
     unless @user == current_user
       redirect_to user_path(current_user)
     end
-  end
   end
 end
