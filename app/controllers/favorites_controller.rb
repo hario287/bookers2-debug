5380d3d349a340d4f2aec6fa1fb78377946d49b1
EC2 Default User <mhyrso27@gmail.com>
2022-10-02 16:29:30 +0000

    [Finish]応用3-いいね機能

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,22 +1,17 @@
 class FavoritesController < ApplicationController
-  before_action :authenticate_user!
 
   def create
-    # @book = Book.find(params[:book_id])
-    # favorite = current_user.favorites.new(book_id: @book.id)
-    # favorite.save
-    @book_favorite = Favorite.new(user_id: current_user.id, book_id: params[:post_id])
-    @book_favorite.save
-    redirect_to book_path(params[:book_id])
+    @book = Book.find(params[:book_id])
+    favorite = current_user.favorites.new(book_id: @book.id)
+    favorite.save
+    redirect_to request.referer
   end
 
   def destroy
-    # @book = Book.find(params[:book_id])
-    # favorite = current_user.favorites.new(book_id: @book.id)
-    # favorite.destroy
-    @book_favorite = Favorite.find_by(user_id: current_user.id, book_id: params[:post_id])
-    @book_favorite.destroy
-    redirect_to book_path(params[:book_id])
+    @book = Book.find(params[:book_id])
+    favorite = current_user.favorites.find_by(book_id: @book.id)
+    favorite.destroy
+    redirect_to request.referer
   end
 
 end
