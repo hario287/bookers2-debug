bdf03b59638825e1025dbbcd4db0251bfea4b10a
EC2 Default User <mhyrso27@gmail.com>
2022-10-02 14:10:38 +0000

    応用3 モデル

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ devise_for :users
   root 'homes#top'
   get "/home/about" => "homes#about"
 
-  resources :books, only: [:index,:show,:edit,:create,:destroy,:update]
+  resources :books, only: [:index,:show,:edit,:create,:destroy,:update]do
+  resource :favorites, only: [:create, :destroy]
+end
   resources :users, only: [:index,:show,:edit,:update]
 end
