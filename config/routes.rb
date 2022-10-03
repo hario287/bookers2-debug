e4c0729c686dcad994102de89519bcde2d7d0c38
EC2 Default User <mhyrso27@gmail.com>
2022-10-03 12:57:13 +0000

    [Finish]応用4

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,10 @@ devise_for :users
   resources :books, only: [:index,:show,:edit,:create,:destroy,:update]do
    resource :favorites, only: [:create, :destroy]
    resources :book_comments, only: [:create, :destroy]
-end
-  resources :users, only: [:index,:show,:edit,:update]
+  end
+  resources :users, only: [:index,:show,:edit,:update]do
+    resource :relationships, only: [:create, :destroy]
+    get 'followings' => 'relationships#followings', as: 'followings'
+    get 'followers' => 'relationships#followers', as: 'followers'
+  end
 end
