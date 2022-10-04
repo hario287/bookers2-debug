b92798e394ea4f67af5ab1a557e2dbf2e4c0e224
EC2 Default User <mhyrso27@gmail.com>
2022-10-04 09:40:59 +0000

    [Add]応用課題5

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,4 +1,17 @@
 class SearchesController < ApplicationController
+  before_action :authenticate_user!
+
   def search
+    @range = params[:range]
+      # 検索モデル→params[:range]
+    if @range == "User"
+      @users = User.looks(params[:search], params[:word])
+    else
+      @books = Book.looks(params[:search], params[:word])
+      # 検索方法→params[:search]
+      # 検索ワード→params[:word]
+    # if文を使い、検索モデルUserorBookで条件分岐
+    # looksメソッドを使い、検索内容を取得し、変数に代入
+    end
   end
 end
