774be03f77703727064f5f69746235d1da819682
EC2 Default User <mhyrso27@gmail.com>
2022-10-07 15:04:35 +0000

    [Finish]応用課題7a

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,7 +3,9 @@ class Book < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
 
-  #いらない？ has_many :follows, dependent: :destroy
+  has_many :favorited_users,through: :favorites, source: :user
+
+  #いらない has_many :follows, dependent: :destroy
 
   validates :title,presence:true
   validates :body,presence:true,length:{maximum:200}
